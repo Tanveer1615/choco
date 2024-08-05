4d337f29ed50dbd2c1723159e1cdf58273a04dc4
Tanveer <ta91507@gmail.com>
2024-08-05 12:00:51 +0530

    seventh

---
+++ src/app/api/orders/history/route.ts
@@ -0,0 +1,42 @@
+import { authOptions } from "@/lib/auth/authOption";
+import { db } from "@/lib/db/db";
+import { orders, products } from "@/lib/db/schema";
+import { eq } from "drizzle-orm";
+import { getServerSession } from "next-auth";
+
+export async function GET() {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return Response.json({ message: "Not allowed" }, { status: 401 });
+  }
+
+  // Number(session.token.id),
+
+  try {
+    const myOrders = await db
+      .select({
+        id: orders.id,
+        product: products.name,
+        type: orders.type,
+        price: orders.price,
+        image: products.image,
+        productDescription: products.description,
+        status: orders.status,
+        address: orders.address,
+        createdAt: orders.createdAt,
+      })
+      .from(orders)
+      .leftJoin(products, eq(orders.productId, products.id))
+      // todo: add types on session
+      // @ts-ignore
+      .where(eq(orders.userId, Number(session.token.id)));
+
+    return Response.json(myOrders);
+  } catch (err) {
+    return Response.json(
+      { message: "Failed to get my orders" },
+      { status: 500 }
+    );
+  }
+}
---
+++ src/app/api/products/route.ts
@@ -0,0 +1,78 @@
+import { productSchema, isServer } from "@/lib/validators/productSchema";
+import { writeFile } from "node:fs/promises";
+import path from "node:path";
+import { db } from "@/lib/db/db";
+import { products } from "@/lib/db/schema";
+import { desc } from "drizzle-orm";
+export async function POST(request: Request) {
+  //   const session = await getServerSession(authOptions);
+
+  //   if (!session) {
+  //     return Response.json({ message: "Not allowed" }, { status: 401 });
+  //   }
+  // todo: check user access.
+  // @ts-ignore
+  //   if (session.token.role !== "admin") {
+  //     return Response.json({ message: "Not allowed" }, { status: 403 });
+  //   }
+
+  const data = await request.formData();
+
+  let validatedData;
+  try {
+    validatedData = productSchema.parse({
+      name: data.get("name"),
+      description: data.get("description"),
+      price: Number(data.get("price")),
+      image: data.get("image"),
+    });
+  } catch (err) {
+    console.log("hii");
+    return Response.json({ message: err }, { status: 400 });
+  }
+
+  const inputImage = validatedData.image;
+  //   isServer
+  //     ? (validatedData.image as File)
+  //     : (validatedData.image as FileList)[0];
+  const filename = `${Date.now()}.${inputImage.name.split(".").slice(-1)}`; // choco.png 213123123123.png
+
+  try {
+    const buffer = Buffer.from(await inputImage.arrayBuffer());
+    await writeFile(
+      path.join(process.cwd(), "public/assets", filename),
+      buffer
+    );
+  } catch (err) {
+    return Response.json(
+      { message: "Failed to save the file to fs" },
+      { status: 500 }
+    );
+  }
+
+  try {
+    await db.insert(products).values({ ...validatedData, image: filename });
+  } catch (err) {
+    // todo: remove stored image from fs
+    return Response.json(
+      { message: "Failed to store product into the database" },
+      { status: 500 }
+    );
+  }
+
+  return Response.json({ message: "OK" }, { status: 201 });
+}
+export async function GET() {
+  try {
+    const allProducts = await db
+      .select()
+      .from(products)
+      .orderBy(desc(products.id));
+    return Response.json(allProducts);
+  } catch (err) {
+    return Response.json(
+      { message: "Failed to fetch products" },
+      { status: 500 }
+    );
+  }
+}
